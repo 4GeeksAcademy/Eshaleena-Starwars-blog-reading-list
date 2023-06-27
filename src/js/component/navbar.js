bbbf80e35bf85098e3916a97999dfe870cee2817
Shaleena Evans <123431776+eshaleena@users.noreply.github.com>
2023-06-27 18:33:15 +0000

    first commit

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -5,7 +5,7 @@ export const Navbar = () => {
 	return (
 		<nav className="navbar navbar-light bg-light mb-3">
 			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+				<span className="navbar-brand mb-0 h1"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRlP_CQFHCMgRWYX4tYGGnTzAw7mprvYrvbq0IVoCsuEA&s" alt="Star Wars logo" /></span>
 			</Link>
 			<div className="ml-auto">
 				<Link to="/demo">
